c4d899de15e47b07bb98d8ad88cc3cdb918a476b
Russ Hyde <russ@jumpingrivers.com>
2023-09-04 15:09:02 +0100

    data: add un_data for use in app

---
+++ R/data.R
@@ -0,0 +1,6 @@
+#' UN country statistics dataset (CC0)
+#'
+#' Obtained from `https://www.kaggle.com/datasets/sudalairajkumar/undata-country-profiles`
+#' on 2023-09-04.
+
+"un_data"
--- data-raw/un_data.R
+++ data-raw/un_data.R
@@ -1,3 +1,5 @@
 ## code to prepare `un_data` dataset goes here
 
+un_data <- readr::read_csv("data-raw/country_profile_variables.csv")
+
 usethis::use_data(un_data, overwrite = TRUE)
