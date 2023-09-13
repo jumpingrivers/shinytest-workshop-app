18e1ebb4d60e0accc7518ae1154d359abca8f37a
Russ Hyde <russ@jumpingrivers.com>
2023-09-13 11:15:03 +0100

    feat: add scatter plot of selected columns

--- R/mod_plots.R
+++ R/mod_plots.R
@@ -4,28 +4,50 @@
 #'
 #' @param id,input,output,session Internal parameters for {shiny}.
 #'
-#' @noRd 
+#' @noRd
 #'
-#' @importFrom shiny NS tagList 
-mod_plots_ui <- function(id){
+#' @importFrom shiny NS tagList
+
+mod_plots_ui <- function(id) {
   ns <- NS(id)
   tagList(
- 
+    plotOutput(ns("scatter"))
   )
 }
-    
+
 #' plots Server Functions
 #'
-#' @noRd 
-mod_plots_server <- function(id){
-  moduleServer( id, function(input, output, session){
+#' @noRd
+
+mod_plots_server <- function(id,
+                             rx_plotting_dataset,
+                             rx_x_var,
+                             rx_y_var) {
+  stopifnot(is.reactive(rx_plotting_dataset))
+  stopifnot(is.reactive(rx_x_var))
+  stopifnot(is.reactive(rx_y_var))
+
+  moduleServer(id, function(input, output, session) {
     ns <- session$ns
- 
+
+    output$scatter <- renderPlot({
+      req(rx_x_var())
+      req(rx_y_var())
+
+      ggplot2::ggplot(
+        data = rx_plotting_dataset(),
+        mapping = ggplot2::aes(
+          x = .data[[rx_x_var()]],
+          y = .data[[rx_y_var()]]
+        )
+      ) +
+        ggplot2::geom_point()
+    })
   })
 }
-    
+
 ## To be copied in the UI
 # mod_plots_ui("plots_1")
-    
+
 ## To be copied in the server
 # mod_plots_server("plots_1")
