735bbfc8b3be66fb8274f77d884b36a7911a6650
Russ Hyde <russ@jumpingrivers.com>
2023-09-13 09:39:41 +0100

    fix: clean_names2 colname_map attribute should map from human-friendly to computer-friendly names

--- R/clean_names.R
+++ R/clean_names.R
@@ -5,14 +5,16 @@
 #' @param   ...   Additional arguments, passed onto `janitor::clean_names`.
 #'
 #' @return   The same data-frame, but with the column names cleaned and an additional attribute
-#' added. The attribute `colname_map` is a named vector containing a mapping between the new (the
-#' names) and old (the values) column names in the data-frame.
+#' added. The attribute `colname_map` is a named vector containing a mapping between the old (the
+#' names) and new (the values) column names in the data-frame. This vector is in the same structure
+#' as expected by 'choices' in selectInput (the human-readable values are the names,
+#' the computer-friendly strings are the values).
 
 clean_names2 <- function(x, ...) {
   cleaned <- janitor::clean_names(x, ...)
 
-  colname_map <- colnames(x)
-  names(colname_map) <- colnames(cleaned)
+  colname_map <- colnames(cleaned)
+  names(colname_map) <- colnames(x)
   attr(cleaned, "colname_map") <- colname_map
 
   cleaned
