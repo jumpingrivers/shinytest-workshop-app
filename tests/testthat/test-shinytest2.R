73e642474cfb55e8dd86094af7b4824beb95afb1
Russ Hyde <russ@jumpingrivers.com>
2023-09-21 13:46:03 +0100

    [FAIL] pass app object into AppDriver
    This fails to get the shinytest2 test to run (unless unstats is
    installed) because are references to unstats::un_data in the app code.
    Any references to the package-under-development can be problematic,
    because the R session where the app runs must have access to the package
    itself.

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -1,7 +1,8 @@
 library(shinytest2)
 
 test_that("Initial Shiny values are consistent", {
-  app <- AppDriver$new()
+  shiny_app <- unstats::run_app()
+  app <- AppDriver$new(shiny_app)
 
   app$expect_values()
 })
