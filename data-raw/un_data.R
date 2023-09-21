229d6629346a287d85c38510f06eed5a0d212fec
Russ Hyde <russ@jumpingrivers.com>
2023-09-21 15:28:27 +0100

    fix: convert missing numerics from -99 to NA at import

--- data-raw/un_data.R
+++ data-raw/un_data.R
@@ -2,8 +2,13 @@
 pkgload::load_all()
 
 un_path <- file.path("data-raw", "country_profile_variables.csv")
+
+# The UN data has encoded all missing numeric values as -99, they are converted to NA at import
 un_data <- un_path |>
-  readr::read_csv(col_types = readr::cols()) |>
+  readr::read_csv(
+    col_types = readr::cols(),
+    na = c("", "NA", "-99")
+  ) |>
   clean_names2()
 
 usethis::use_data(un_data, overwrite = TRUE)
