5d4d55c612664a24c6464f3b785441b2f71f5ffb
Russ Hyde <russ@jumpingrivers.com>
2023-09-21 13:52:10 +0100

    [FIX] tests use app.R to start the app
    This circumvents problems with loading the dev version of the package
    into the session where the app runs during tests, because it uses
    pkgload::load_all() in the app.R file.

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -1,8 +1,7 @@
 library(shinytest2)
 
 test_that("Initial Shiny values are consistent", {
-  shiny_app <- unstats::run_app()
-  app <- AppDriver$new(shiny_app)
+  app <- AppDriver$new()
 
   app$expect_values()
 })
