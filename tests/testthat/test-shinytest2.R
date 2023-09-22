7daad84081acc0b113af4081829d2374d1e4b3f0
Russ Hyde <russ@jumpingrivers.com>
2023-09-22 13:27:35 +0100

    [REVERT]: don't use app.R as test entry-point, it's absent during R CMD check
    But always remember to 'install' your package before using
    devtools::test()

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -1,7 +1,8 @@
 library(shinytest2)
 
 test_that("Initial Shiny values are consistent", {
-  app <- AppDriver$new()
+  shiny_obj <- unstats::run_app()
+  app <- AppDriver$new(shiny_obj)
 
   app$expect_values()
 })
