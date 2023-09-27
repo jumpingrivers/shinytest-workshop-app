6285aee8894c0cf27829bef681d01fd4ffb0cd24
Russ Hyde <russ@jumpingrivers.com>
2023-09-27 16:55:37 +0100

    fix(test): more robust, deduplicated test for selecting regions

--- tests/testthat/test-e2e-region_selection.R
+++ tests/testthat/test-e2e-region_selection.R
@@ -2,11 +2,12 @@ library(shinytest2)
 
 test_that("data filtering by region", {
   shiny_obj <- unstats::run_app()
-  app <- AppDriver$new(shiny_obj)
+  app <- AppDriver$new(shiny_obj, width = 1062, height = 904)
 
-  app$set_window_size(width = 1062, height = 904)
-  app$set_inputs(`sidebar-countries-regions-user_selected` = "Caribbean")
-  app$set_inputs(`sidebar-countries-regions-user_selected` = c("Caribbean", "EasternAfrica"))
-  app$set_inputs(`sidebar-countries-regions-user_selected` = c("Caribbean", "EasternAfrica", "EasternEurope"))
+  app$set_inputs(
+    `sidebar-countries-regions-user_selected` = c("Caribbean", "EasternAfrica", "EasternEurope")
+  )
+
+  app$wait_for_idle(500)
   app$expect_values()
 })
